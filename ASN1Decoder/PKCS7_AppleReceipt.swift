c11caa21bb061f9fb88c35c11a2a3573f45faf1d
filom <maguolo.ios@outlook.com>
2019-01-14 14:55:46 +0100

    Completed parse of receipt including in-app purchases

--- ASN1Decoder/PKCS7_AppleReceipt.swift
+++ ASN1Decoder/PKCS7_AppleReceipt.swift
@@ -23,67 +23,126 @@
 
 import Foundation
 
+/*
+ This extension allow to parse the content of an Apple receipt from the AppStore.
+ 
+ Reference documentation
+ https://developer.apple.com/library/archive/releasenotes/General/ValidateAppStoreReceipt/Chapters/ReceiptFields.html
+ */
 extension PKCS7 {
     
-    public class ReceiptInfo {
-        var bundleIdentifier: String?
-        var bundleVersion: String?
-        var originalApplicationVersion: String?
-        var receiptCreationDate: Date?
-        var receiptCreationDateString: String?
-        var receiptExpirationDate: Date?
-        var receiptExpirationDateString: String?
+    public struct ReceiptInfo {
+        
+        /// CFBundleIdentifier in Info.plist
+        public fileprivate(set) var bundleIdentifier: String?
+        
+        /// CFBundleVersion (in iOS) or CFBundleShortVersionString (in macOS) in Info.plist
+        public fileprivate(set) var bundleVersion: String?
+        
+        /// CFBundleVersion (in iOS) or CFBundleShortVersionString (in macOS) in Info.plist
+        public fileprivate(set) var originalApplicationVersion: String?
+        
+        public fileprivate(set) var receiptCreationDate: Date?
+        public fileprivate(set) var receiptCreationDateString: String?
+        public fileprivate(set) var receiptExpirationDate: Date?
+        public fileprivate(set) var receiptExpirationDateString: String?
+        public fileprivate(set) var inAppPurchases: [InAppPurchaseInfo]?
     }
     
+    public struct InAppPurchaseInfo {
+        public fileprivate(set) var quantity: UInt64?
+        public fileprivate(set) var productId: String?
+        public fileprivate(set) var transactionId: String?
+        public fileprivate(set) var originalTransactionId: String?
+        public fileprivate(set) var purchaseDate: Date?
+        public fileprivate(set) var originalPurchaseDate: Date?
+        public fileprivate(set) var expiresDate: Date?
+        public fileprivate(set) var isInIntroOfferPeriod: UInt64?
+        public fileprivate(set) var cancellationDate: Date?
+        public fileprivate(set) var webOrderLineItemId: UInt64?
+    }
+    
+    
     public func receipt() -> ReceiptInfo? {
+        guard let block = mainBlock.findOid(OID_Data) else { return nil }
+        guard let receiptBlock = block.parent?.sub?.last?.sub(0)?.sub(0) else { return nil }
+        var receiptInfo = ReceiptInfo()
         
-        if let block = mainBlock.findOid(OID_Data) {
-            
-            if let receiptBlock = block.parent?.sub?.last?.sub(0)?.sub(0) {
+        let parseDate: (String) -> Date? = { dateString in
+            let rfc3339DateFormatter = DateFormatter()
+            rfc3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            rfc3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+            rfc3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+            return rfc3339DateFormatter.date(from: dateString)
+        }
+        
+        for item in receiptBlock.sub ?? [] {
+            let fieldType = (item.sub(0)?.value as? Data)?.toIntValue() ?? 0
+            let fieldValueString = item.sub(2)?.sub?.first?.value as? String
+            switch fieldType {
+            case 2:
+                receiptInfo.bundleIdentifier = fieldValueString
                 
-                let receiptInfo = ReceiptInfo()
+            case 3:
+                receiptInfo.bundleVersion = fieldValueString
                 
-                let parseDate: (String) -> Date? = { dateString in
-                    let rfc3339DateFormatter = DateFormatter()
-                    rfc3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
-                    rfc3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-                    rfc3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-                    return rfc3339DateFormatter.date(from: dateString)
-                }
+            case 19:
+                receiptInfo.originalApplicationVersion = fieldValueString
                 
-                for item in receiptBlock.sub ?? [] {
-                    
-                    let fieldType = (item.sub(0)?.value as? Data)?.toIntValue() ?? 0
-                    
-                    guard let fieldValue = item.sub(2)?.sub?.first?.value as? String else {
-                        continue
-                    }
-                    
+            case 12:
+                guard let fieldValueString = fieldValueString else { continue }
+                receiptInfo.receiptCreationDateString = fieldValueString
+                receiptInfo.receiptCreationDate = parseDate(fieldValueString)
+                
+            case 21:
+                guard let fieldValueString = fieldValueString else { continue }
+                receiptInfo.receiptExpirationDateString = fieldValueString
+                receiptInfo.receiptExpirationDate = parseDate(fieldValueString)
+                
+            case 17:
+                var inAppPurchaseInfo = InAppPurchaseInfo()
+                for subItem in item.sub(2)?.sub?.first?.sub ?? [] {
+                    let fieldType = (subItem.sub(0)?.value as? Data)?.toIntValue() ?? 0
+                    let fieldValue = subItem.sub(2)?.sub?.first?.value
                     switch fieldType {
-                    case 2:
-                        receiptInfo.bundleIdentifier = fieldValue
-                        
-                    case 3:
-                        receiptInfo.bundleVersion = fieldValue
-                        
-                    case 19:
-                        receiptInfo.originalApplicationVersion = fieldValue
-                        
-                    case 12:
-                        receiptInfo.receiptCreationDateString = fieldValue
-                        receiptInfo.receiptCreationDate = parseDate(fieldValue)
-                        
-                    case 21:
-                        receiptInfo.receiptExpirationDateString = fieldValue
-                        receiptInfo.receiptExpirationDate = parseDate(fieldValue)
-                        
-                    default: break
+                    case 1701:
+                        inAppPurchaseInfo.quantity = (fieldValue as? Data)?.toIntValue()
+                    case 1702:
+                        inAppPurchaseInfo.productId = fieldValue as? String
+                    case 1703:
+                        inAppPurchaseInfo.transactionId = fieldValue as? String
+                    case 1705:
+                        inAppPurchaseInfo.originalTransactionId = fieldValue as? String
+                    case 1704:
+                        guard let fieldValueString = fieldValue as? String else { continue }
+                        inAppPurchaseInfo.purchaseDate = parseDate(fieldValueString)
+                    case 1706:
+                        guard let fieldValueString = fieldValue as? String else { continue }
+                        inAppPurchaseInfo.originalPurchaseDate = parseDate(fieldValueString)
+                    case 1708:
+                        guard let fieldValueString = fieldValue as? String else { continue }
+                        inAppPurchaseInfo.expiresDate = parseDate(fieldValueString)
+                    case 1719:
+                        inAppPurchaseInfo.isInIntroOfferPeriod = (fieldValue as? Data)?.toIntValue()
+                    case 1712:
+                        guard let fieldValueString = fieldValue as? String else { continue }
+                        inAppPurchaseInfo.cancellationDate = parseDate(fieldValueString)
+                    case 1711:
+                        inAppPurchaseInfo.webOrderLineItemId = (fieldValue as? Data)?.toIntValue()
+                    default:
+                        break
                     }
                 }
-                return receiptInfo
+                if receiptInfo.inAppPurchases == nil {
+                    receiptInfo.inAppPurchases = []
+                }
+                receiptInfo.inAppPurchases?.append(inAppPurchaseInfo)
+                
+            default:
+                break
             }
         }
-        return nil
+        return receiptInfo
     }
     
 }
