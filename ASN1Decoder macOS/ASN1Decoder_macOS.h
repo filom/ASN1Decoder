d82d548f7fc5d7f0868c6ba3bca188cd84ca1f4a
filom <maguolo.ios@outlook.com>
2019-08-18 15:22:09 +0200

    Fix support for macOS Carthage build

---
+++ ASN1Decoder macOS/ASN1Decoder_macOS.h
@@ -0,0 +1,19 @@
+//
+//  ASN1Decoder_macOS.h
+//  ASN1Decoder macOS
+//
+//  Created by Filippo Maguolo on 18/08/2019.
+//  Copyright © 2019 Filippo Maguolo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ASN1Decoder_macOS.
+FOUNDATION_EXPORT double ASN1Decoder_macOSVersionNumber;
+
+//! Project version string for ASN1Decoder_macOS.
+FOUNDATION_EXPORT const unsigned char ASN1Decoder_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASN1Decoder_macOS/PublicHeader.h>
+
+
