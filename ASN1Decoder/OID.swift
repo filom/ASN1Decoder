fd792c8f74763b4ee18dd381293af18bbf3faf9e
Alexander Heinrich <aheinrich@seemoo.tu-darmstadt.de>
2020-06-19 14:26:24 +0200

    Adding support for accessing signatures and signer info from a PKCS7 encoded file

--- ASN1Decoder/OID.swift
+++ ASN1Decoder/OID.swift
@@ -21,6 +21,16 @@ public enum OID: String {
     case sha256WithRSAEncryption = "1.2.840.113549.1.1.11"
     case md5WithRSAEncryption = "1.2.840.113549.1.1.4"
     case sha1WithRSAEncryption = "1.2.840.113549.1.1.5"
+    
+    //Digest algorithms
+    case sha1 = "1.3.14.3.2.26"
+    case pkcsSha256 = "1.3.6.1.4.1.22554.1.2.1"
+    case sha2Family = "1.3.6.1.4.1.22554.1.2"
+    case sha3_244 = "2.16.840.1.101.3.4.2.7"
+    case sha3_256 = "2.16.840.1.101.3.4.2.8"
+    case sha3_384 = "2.16.840.1.101.3.4.2.9"
+    case md5 = "0.2.262.1.10.1.3.2"
+    
     case pkcs7data = "1.2.840.113549.1.7.1"
     case pkcs7signedData = "1.2.840.113549.1.7.2"
     case pkcs7envelopedData = "1.2.840.113549.1.7.3"
--- ASN1Decoder/PKCS7_Signature.swift
+++ ASN1Decoder/PKCS7_Signature.swift
@@ -8,19 +8,77 @@
 import Foundation
 
 extension PKCS7 {
-    public var signatures: [Data]? {
-        guard let block = mainBlock.findOid(.pkcs7signedData) else {return nil}
-        
+    public var signatures: [SignatureInfo]? {
         //Signer infos sequence. https://tools.ietf.org/html/rfc5652#section-5.3
-        guard let signerInfos = block.sub(5) else {return nil}
+        
+        guard let signerInfos = mainBlock.sub(4) else {return nil}
+        
         let numberOfSignatures = signerInfos.subCount()
         
-        #if DEBUG
-        print(numberOfSignatures)
-        print(signerInfos.description)
-        #endif
+        var signatures = [SignatureInfo]()
         
-        return nil
+        for i in 0..<numberOfSignatures {
+            guard let signatureInfoasn1 = signerInfos.sub(i) else {continue}
+            let sigInfo = SignatureInfo(asn1: signatureInfoasn1)
+            signatures.append(sigInfo)
+        }
         
+        
+        
+        return signatures
+        
+    }
+}
+
+/// Signature info for a PKCS7 file
+/// https://tools.ietf.org/html/rfc5652#section-5.3
+public class SignatureInfo {
+    
+    /// The version of the Signature info. Should be 1
+    public let version: ASN1Object?
+    /// Contains information about the signing certificate, like commonName, OU, organization, country
+    public let signerIdentifier: ASN1Object?
+    /// The identifier for the algorithm that was used to sign
+    public let digestAlgorithmIdentifier: ASN1Object?
+    /// Contains information how the signature was created. A set with a sequence. First entry is the hashing algorithm. Second is the signature algorithm (not sure if this is valid for all pkcs files)
+    public let signedAttributes: ASN1Object?
+    /// Algorithm used to create the signature
+    public let signatureAlgorithm: ASN1Object?
+    /// The actual signature
+    public let signature: ASN1Object?
+    
+    public var signatureData: Data? {
+        return signature?.value as? Data
+    }
+    
+    public var disgestAlgorithmName: String? {
+        guard let oid = self.digestAlgorithmOID else {return nil}
+        
+        return String(describing: oid)
+    }
+    
+    public var digestAlgorithmOID: OID? {
+        let value = digestAlgorithmIdentifier?.sub(0)?.value as? String ?? ""
+        return OID(rawValue: value)
+    }
+    
+    public var signatureAlgorithmName: String? {
+        guard let oid = self.signatureAlgorithmOID else {return nil}
+        
+        return String(describing: oid)
+    }
+    
+    public var signatureAlgorithmOID: OID? {
+        let value = signatureAlgorithm?.sub(0)?.value as? String ?? ""
+        return OID(rawValue: value)
+    }
+    
+    public init(asn1: ASN1Object) {
+        version = asn1.sub(0)
+        signerIdentifier = asn1.sub(1)
+        digestAlgorithmIdentifier = asn1.sub(2)
+        signedAttributes = asn1.sub(3)
+        signatureAlgorithm = asn1.sub(4)
+        signature = asn1.sub(5)
     }
 }
