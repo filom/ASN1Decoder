d45311aabed66b0b8777fd859604c8d98a7bcb7a
Hannes Oud <hannes.oud.dev@gmail.com>
2020-10-16 15:37:11 +0200

    Add fallbackDateFormatterWithMS to support potential apple bugs with millisecond-granularity dates

--- ASN1Decoder/PKCS7_AppleReceipt.swift
+++ ASN1Decoder/PKCS7_AppleReceipt.swift
@@ -173,8 +173,10 @@ extension PKCS7 {
 private enum ReceiptDateFormatter {
 
     /// Uses receipt-conform representation of dates like "2017-01-01T12:00:00Z",
+    /// as a fallback, dates like "2017-01-01T12:00:00.123Z" are also parsed.
     static func date(from string: String) -> Date? {
         return self.defaultDateFormatter.date(from: string) // expected
+            ?? self.fallbackDateFormatterWithMS.date(from: string) // try again with milliseconds
     }
 
     /// Uses receipt-conform representation of dates like "2017-01-01T12:00:00Z" (rfc3339 without millis)
@@ -184,5 +186,19 @@ private enum ReceiptDateFormatter {
         dateDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         dateDateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
         return dateDateFormatter
+    }()
+
+    /// Uses representation of dates like "2017-01-01T12:00:00.123Z"
+    ///
+    /// This is not the officially intended format, but added after hearing reports about new format adding ms https://twitter.com/depth42/status/1314179654811607041
+    ///
+    /// The formatting String was taken from https://github.com/IdeasOnCanvas/AppReceiptValidator/pull/73
+    /// where tests were performed to check if it works
+    private static let fallbackDateFormatterWithMS: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        return dateFormatter
     }()
 }
