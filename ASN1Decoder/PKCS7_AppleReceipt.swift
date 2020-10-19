80aed138e91fb61356b0e276b8e5b7574b66f8ce
Hannes Oud <hannes.oud.dev@gmail.com>
2020-10-19 13:26:14 +0200

    Fix missing ` in fallback date format

--- ASN1Decoder/PKCS7_AppleReceipt.swift
+++ ASN1Decoder/PKCS7_AppleReceipt.swift
@@ -197,7 +197,7 @@ private enum ReceiptDateFormatter {
     private static let fallbackDateFormatterWithMS: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
+        dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"
         dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
         return dateFormatter
     }()
